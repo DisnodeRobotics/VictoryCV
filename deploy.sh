7fe12d4d9b28cdbecad1d8d55676ccac94973451
Alex Carter <accounts@atecstudios.com>
2018-02-25 21:19:16 -0800

    Deploy Change

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ﻿echo "Deploying to PI"
 sshpass -p "raspberry" scp -r /home/alex/Projects/FRC/VictoryCV/src pi@pi3-01.local:/home/pi/projects/FRC/VictoryCV
-sshpass -p "raspberry" ssh pi@pi3-01.local "cd /home/pi/projects/FRC/VictoryCV && make"
+sshpass -p "raspberry" ssh pi@pi3-01.local "cd /home/pi/projects/FRC/VictoryCV && cmake . && make"
